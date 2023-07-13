8988a71d00277d021f005d4738b3c101c9f62f4c
Olivia Gilbert <umhello2020@gmail.com>
2023-07-12 22:10:49 -0500

    added animation and text to the page and started to create the logo component

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,6 +3,7 @@ import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope, faHome, faUser  } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
@@ -22,6 +23,18 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
+            <ul>
+                <li>
+                    <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/olivia-gilbert-519258265/'>
+                        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel='noreferrer' href='https://www.github.com/umhello2020'>
+                        <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                    </a>
+                </li>
+            </ul>
     </div>
 )
 
